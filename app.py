3068115c262dc20386cc3ff6393d0307282c64bf
Osman Batuhan Şahin <60979988+osmanbatuhansahin@users.noreply.github.com>
2022-06-18 18:11:36 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -22,6 +22,12 @@ def ValuePredictor(to_predict_list):
     result = loaded_model.predict(to_predict)
     return result[0]
 
+def ValuePredictor2(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1,8)
+    loaded_model = pickle.load(open("model.pkl","rb"))
+    result2 = loaded_model.predict_proba(to_predict)
+    return result2
+
 
 @app.route('/result',methods = ['POST'])
 def result():
@@ -30,13 +36,15 @@ def result():
         to_predict_list=list(to_predict_list.values())
         to_predict_list = list(map(int, to_predict_list))
         result = ValuePredictor(to_predict_list)
-        
+        result2 = ValuePredictor2(to_predict_list)
+        result2 = result2.tolist()
         if int(result)==1:
-            prediction="%97 ihtimalle diyabetsin,"
+            
+            prediction="%"+str(int(100* result2[0][1]))+" ihtimalle diyabetsin,"
             prediction2="bir doktora görünmelisin."
 
         else:
-            prediction='%97 ihtimalle sağlıklısın.'
+            prediction="%"+str(int(100* result2[0][0]))+" ihtimalle sağlıklısın."
             prediction2=""
             
         return render_template("result.html",prediction=prediction,prediction2=prediction2)
